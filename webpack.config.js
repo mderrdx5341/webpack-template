978fdc27d676fcacfa2da2158b227cc5b1f4c00a
Roman Verkeev <mderrdx5341@gmail.com>
2025-08-06 21:21:28 +0300

    update webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,50 @@
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+//const { ProvidePlugin } = require("webpack");
 const path = require('path');
 
 module.exports = {
+    externals: {
+        jquery : 'jQuery',
+    },
     plugins: [
         new MiniCssExtractPlugin({
             filename: '[name].css',
-		})
+		}),
+        /*new ProvidePlugin({
+            $: "jquery",
+            jQuery: "jquery",
+
+        })*/
 	],
 	entry: {
 		app: path.resolve(__dirname, './frontend/main.js'),
 	},
     devtool: 'source-map',
-    mode: "production",
+    mode: "development", //production, development
+    stats: {
+        errorDetails: true
+    },
     resolve: {
         extensions: ['.tsx', '.ts', '.js']
     },
     module: {
         rules: [
+            {
+                test: /\.(png|jpg|jpeg|svg|gif)$/,
+                type: 'asset/resource',
+                generator: {
+                    // Optional: Customize the output filename and path for the SVG files
+                    filename: 'images/[name][ext][query]',
+                },
+            },
+            {
+                test: /\.(eot|ttf|woff|woff2)$/,
+                type: 'asset/resource',
+                generator: {
+                    // Optional: Customize the output filename and path for the SVG files
+                    filename: 'fonts/[name][ext][query]',
+                },
+            },
             {
                 test: /\.js$/,
                 exclude: /node_modules/,
@@ -30,7 +58,8 @@ module.exports = {
                 test: /\.tsx?$/,
                 loader: 'ts-loader',
                 exclude: /node_modules/,
-            }
+            },
+
         ],
 	},
 	output: {
